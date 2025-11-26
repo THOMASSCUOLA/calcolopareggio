563812d49281f14191a7e49a9709ff9b9bea905f
TARTARI.21116 <TARTARI.21116@LABI1-13PC25>
2025-11-26 13:13:56 +0100

    agg

--- calcolopareggio/Form1.Designer.cs
+++ calcolopareggio/Form1.Designer.cs
@@ -28,38 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdomandafissa = new System.Windows.Forms.TextBox();
+            this.txtoffertafissa = new System.Windows.Forms.TextBox();
+            this.txtcoefficenteofferta = new System.Windows.Forms.TextBox();
+            this.txtcoefficenteDomanda = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtcoefficenteEsponente = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(489, 30);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series3.Legend = "Legend1";
-            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
-            series3.Name = "Domanda";
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
-            series4.Name = "Offerta";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Domanda";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series2.Name = "Offerta";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(491, 379);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
@@ -72,10 +82,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(31, 30);
+            this.dataGridView1.Location = new System.Drawing.Point(31, 109);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(452, 379);
+            this.dataGridView1.Size = new System.Drawing.Size(452, 300);
             this.dataGridView1.TabIndex = 4;
             // 
             // button1
@@ -88,11 +98,101 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Offerta fissa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Domanda Fissa";
+            // 
+            // txtdomandafissa
+            // 
+            this.txtdomandafissa.Location = new System.Drawing.Point(126, 6);
+            this.txtdomandafissa.Name = "txtdomandafissa";
+            this.txtdomandafissa.Size = new System.Drawing.Size(100, 20);
+            this.txtdomandafissa.TabIndex = 8;
+            // 
+            // txtoffertafissa
+            // 
+            this.txtoffertafissa.Location = new System.Drawing.Point(344, 6);
+            this.txtoffertafissa.Name = "txtoffertafissa";
+            this.txtoffertafissa.Size = new System.Drawing.Size(100, 20);
+            this.txtoffertafissa.TabIndex = 9;
+            // 
+            // txtcoefficenteofferta
+            // 
+            this.txtcoefficenteofferta.Location = new System.Drawing.Point(344, 32);
+            this.txtcoefficenteofferta.Name = "txtcoefficenteofferta";
+            this.txtcoefficenteofferta.Size = new System.Drawing.Size(100, 20);
+            this.txtcoefficenteofferta.TabIndex = 13;
+            // 
+            // txtcoefficenteDomanda
+            // 
+            this.txtcoefficenteDomanda.Location = new System.Drawing.Point(126, 32);
+            this.txtcoefficenteDomanda.Name = "txtcoefficenteDomanda";
+            this.txtcoefficenteDomanda.Size = new System.Drawing.Size(100, 20);
+            this.txtcoefficenteDomanda.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "coefficente ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Coefficente";
+            // 
+            // txtcoefficenteEsponente
+            // 
+            this.txtcoefficenteEsponente.Location = new System.Drawing.Point(344, 58);
+            this.txtcoefficenteEsponente.Name = "txtcoefficenteEsponente";
+            this.txtcoefficenteEsponente.Size = new System.Drawing.Size(100, 20);
+            this.txtcoefficenteEsponente.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(263, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Esponente";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1010, 573);
+            this.Controls.Add(this.txtcoefficenteEsponente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtcoefficenteofferta);
+            this.Controls.Add(this.txtcoefficenteDomanda);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtoffertafissa);
+            this.Controls.Add(this.txtdomandafissa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
@@ -101,6 +201,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +210,16 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdomandafissa;
+        private System.Windows.Forms.TextBox txtoffertafissa;
+        private System.Windows.Forms.TextBox txtcoefficenteofferta;
+        private System.Windows.Forms.TextBox txtcoefficenteDomanda;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtcoefficenteEsponente;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- calcolopareggio/Form1.cs
+++ calcolopareggio/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -16,115 +10,89 @@ namespace calcolopareggio
         public Form1()
         {
             InitializeComponent();
-            // Initialize columns for dataGridView
+
             dataGridView1.Columns.Clear();
             dataGridView1.Columns.Add("quantità", "quantità");
-            dataGridView1.Columns.Add("ddomanda", "domanda");
+            dataGridView1.Columns.Add("domanda", "domanda");
             dataGridView1.Columns.Add("offerta", "offerta");
         }
 
-     
-
         private void button1_Click_1(object sender, EventArgs e)
         {
-            // Clear previous results
+            // Clear rows
             dataGridView1.Rows.Clear();
+
+            // =============================
+            // LETTURA PARAMETRI DINAMICI
+            // =============================
+            double a = double.Parse(txtdomandafissa.Text);        // domanda costante
+            double b = double.Parse(txtcoefficenteDomanda.Text);  // coeff domanda
+
+            double c = double.Parse(txtoffertafissa.Text);        // offerta costante
+            double k = double.Parse(txtcoefficenteofferta.Text);  // coeff offerta (CORRETTO)
+            int exp = int.Parse(txtcoefficenteEsponente.Text);    // esponente offerta
 
             // valori iniziali
             double q = 0.0;
-            double d = 90 - 4 * q;
-            double o = 10 + Math.Pow(q, 3) / 100.0;
+            double step = 0.01;
 
-            double dIniziale = 90;
-            double oIniziale = 10;
+            double d = a - b * q;
+            double o = c + k * Math.Pow(q, exp);
 
-            dataGridView1.Rows.Add(q, d, o);
+            dataGridView1.Rows.Add(
+     Math.Round(q, 3),
+     Math.Round(d, 3),
+     Math.Round(o, 3)
+ );
 
-            // -------------------------------
-            // FASE 1: ricerca precisa del punto di pareggio
-            // -------------------------------
 
-            double step = 1.0;      // passo grande all'inizio
-            double minStep = 0.0001;
-            bool superato = false;
+            // =============================
+            // CERCA PUNTO DI EQUILIBRIO
+            // =============================
+            bool equilibrioTrovato = false;
+            double equilibrioQ = 0, equilibrioD = 0, equilibrioO = 0;
 
-            int safetyLimit = 500000;
-            int counter = 0;
+            int safety = 500000;
+            int count = 0;
 
-            while (!superato && counter < safetyLimit)
+            while (count < safety)
             {
-                counter++;
+                count++;
 
                 q += step;
+                d = a - b * q;
+                o = c + k * Math.Pow(q, exp);
 
-                d = 90 - 4 * q;
-                o = 10 + Math.Pow(q, 3) / 100.0;
-
-                // Add initial row
                 dataGridView1.Rows.Add(q, d, o);
 
-                double diff = o - d;   // positivo = o sopra d
-
-                // Riduzione dinamica step per precisione crescente
-                if (Math.Abs(diff) < 10) step = 0.1;
-                if (Math.Abs(diff) < 1) step = 0.01;
-                if (Math.Abs(diff) < 0.1) step = 0.001;
-                if (Math.Abs(diff) < 0.01) step = minStep;
-
-                // ---> se superato, passa alla FASE 2
-                if (o >= d)
+                if (o >= d && !equilibrioTrovato)
                 {
-                    superato = true;
+                    equilibrioTrovato = true;
+                    equilibrioQ = q;
+                    equilibrioD = d;
+                    equilibrioO = o;
+                    break;
                 }
             }
 
-            // -------------------------------
-            // FASE 2: arrivo fino a ~13.9 poi passo diretto a 14 e continuo +1
-            // -------------------------------
-
-            double qLimit = 13.9;      // limite prima di forzare a 14
-            double qTarget = Math.Ceiling(q);  // di solito sarà 14 se sei a 13.x
-
-            // 1) Incrementi piccoli SOLO finché q < 13.9
-            while (q < qLimit && counter < safetyLimit)
+            // =============================
+            // AGGIUNGO PUNTI DOPO EQUILIBRIO
+            // =============================
+            for (int i = 0; i < 50; i++)
             {
-                counter++;
-
-                q += 0.1;     // passo piccolo
-                d = 90 - 4 * q;
-                o = 10 + Math.Pow(q, 3) / 100.0;
+                q += step;
+                d = a - b * q;
+                o = c + k * Math.Pow(q, exp);
 
                 dataGridView1.Rows.Add(q, d, o);
             }
 
-            // 2) Appena supero 13.9 → salto diretto al successivo intero
-            q = qTarget;  // esempio: si fissa a 14
-            d = 90 - 4 * q;
-            o = 10 + Math.Pow(q, 3) / 100.0;
-            dataGridView1.Rows.Add(q, d, o);
-
-            // 3) Ora incremento regolare di 1 come volevi
-            while (d > oIniziale && counter < safetyLimit)
-            {
-                counter++;
-
-                q += 1;   // passo regolare da 1
-
-                d = 90 - 4 * q;
-                o = 10 + Math.Pow(q, 3) / 100.0;
-
-                dataGridView1.Rows.Add(q, d, o);
-            }
-
-            // ==============================================
-            // CREAZIONE DEL GRAFICO
-            // ==============================================
-
-            // Svuoto eventuali dati precedenti
+            // =============================
+            // GRAFICO
+            // =============================
             chart1.Series["Domanda"].Points.Clear();
             chart1.Series["Offerta"].Points.Clear();
 
-            // Popolo il grafico leggendo i valori dalla DataGridView
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
                 if (row.Cells[0].Value == null) continue;
@@ -135,50 +103,20 @@ namespace calcolopareggio
 
                 chart1.Series["Domanda"].Points.AddXY(qVal, dVal);
                 chart1.Series["Offerta"].Points.AddXY(qVal, oVal);
-                // Arrotondo solo i valori visibili sugli assi del grafico
-                chart1.ChartAreas[0].AxisX.LabelStyle.Format = "0";     // solo interi
-                chart1.ChartAreas[0].AxisY.LabelStyle.Format = "0.##";  // massimo due decimali
-
-
             }
 
-            // Miglioro l’aspetto come nel grafico Excel
             chart1.ChartAreas[0].AxisX.Title = "quantità";
             chart1.ChartAreas[0].AxisY.Title = "prezzo";
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "0.##";
+            chart1.ChartAreas[0].AxisY.LabelStyle.Format = "0.##";
+
             chart1.Titles.Clear();
-            chart1.Titles.Add("aggiornamento sw"); // titolo come nell’esempio
-            chart1.ChartAreas[0].RecalculateAxesScale();
+            chart1.Titles.Add("aggiornamento sw");
 
             // =============================
-            // PUNTO DI EQUILIBRIO
+            // PUNTO DI EQUILIBRIO (pallino rosso)
             // =============================
-
-            // Cerco la prima riga in cui o >= d
-            double equilibrioQ = 0;
-            double equilibrioD = 0;
-            double equilibrioO = 0;
-            bool trovato = false;
-
-            foreach (DataGridViewRow row in dataGridView1.Rows)
-            {
-                if (row.Cells[0].Value == null)
-                    continue;
-
-                double qVal = Convert.ToDouble(row.Cells[0].Value);
-                double dVal = Convert.ToDouble(row.Cells[1].Value);
-                double oVal = Convert.ToDouble(row.Cells[2].Value);
-
-                if (oVal >= dVal && !trovato)
-                {
-                    equilibrioQ = qVal;
-                    equilibrioD = dVal;
-                    equilibrioO = oVal;
-                    trovato = true;
-                }
-            }
-
-            // Se trovato, metto il pallino rosso
-            if (trovato)
+            if (equilibrioTrovato)
             {
                 var serieEquilibrio = chart1.Series.Add("Equilibrio");
                 serieEquilibrio.ChartType = SeriesChartType.Point;
@@ -188,13 +126,13 @@ namespace calcolopareggio
 
                 serieEquilibrio.Points.AddXY(equilibrioQ, equilibrioD);
 
-                // Etichetta vicino al punto
-                serieEquilibrio.Points[0].Label = $"q={Math.Round(equilibrioQ, 5)}\nd={Math.Round(equilibrioD, 5)}\no={Math.Round(equilibrioO, 5)}";
+                serieEquilibrio.Points[0].Label =
+                    $"q={Math.Round(equilibrioQ, 5)}\n" +
+                    $"d={Math.Round(equilibrioD, 5)}\n" +
+                    $"o={Math.Round(equilibrioO, 5)}";
+
                 serieEquilibrio.Points[0].LabelForeColor = Color.Black;
-
             }
-
-
         }
     }
 }
